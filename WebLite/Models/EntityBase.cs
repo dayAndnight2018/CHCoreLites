cef91c5372e3e05e94af27231080ca839a829023
Cheng Hua <44399091+dayAndnight2018@users.noreply.github.com>
2019-06-04 11:41:50 +0800

    Add files via upload

---
+++ WebLite/Models/EntityBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebLite.Models
+{
+    public class EntityBase
+    {
+        public long Id { get; set; }
+
+        public DateTime CreateTime { get; set; }
+
+        public bool Deleted { get; set; }
+    }
+}
---
+++ WebLite/Models/UserBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebLite.Models
+{
+    public class UserBase : EntityBase
+    {
+        public string Token { get; set; }
+
+        public string ExpiresTime { get; set; }
+
+        public long RoleId { get; set; }
+
+        public UserRole Role { get; set; }
+    }
+}
