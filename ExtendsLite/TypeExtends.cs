4afa9b7ebff04e3b146181614da0bbfad8c72500
Cheng Hua <44399091+dayAndnight2018@users.noreply.github.com>
2019-06-04 11:49:19 +0800

    Add files via upload

---
+++ ExtendsLite/TypeExtends.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtendsLite
+{
+    public static class TypeExtends
+    {
+        /// <summary>
+        /// 判断类型是否是內建的简单类型
+        /// </summary>
+        public static bool IsSimpleType(this Type type)
+        {
+            if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>))
+                type = type.GetGenericArguments()[0]; // or  Nullable.GetUnderlyingType(type)
+
+            if (type.IsEnum)
+                return true;
+
+            if (type == typeof(Guid))
+                return true;
+
+            TypeCode tc = Type.GetTypeCode(type);
+            switch (tc)
+            {
+                case TypeCode.Byte:
+                case TypeCode.SByte:
+                case TypeCode.Int16:
+                case TypeCode.Int32:
+                case TypeCode.Int64:
+                case TypeCode.UInt16:
+                case TypeCode.UInt32:
+                case TypeCode.UInt64:
+                case TypeCode.Single:
+                case TypeCode.Double:
+                case TypeCode.Decimal:
+                case TypeCode.Char:
+                case TypeCode.String:
+                case TypeCode.Boolean:
+                case TypeCode.DateTime:
+                    return true;
+                case TypeCode.Object:
+                    return (typeof(TimeSpan) == type) || (typeof(DateTimeOffset) == type);
+                default:
+                    return false;
+            }
+        }
+    }
+}
---
+++ IDGeneratorLite/IdGenerateFactory.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IDGeneratorLite
+{
+    /// <summary>
+    /// ID生成器
+    /// </summary>
+    public class IDGenerateFactory
+    {
+        private static IDGenerator generator;
+
+        private static object LOCK = new object();
+
+        private static IDGenerator GetInstance()
+        {
+            if(generator == null)
+            {
+                lock(LOCK)
+                {
+                    if(generator ==null)
+                    {
+                        generator = new IDGenerator(sequenceBits: 20);
+                    }
+                }
+            }
+
+            return generator;
+        }
+
+        /// <summary>
+        /// 生成id
+        /// </summary>
+        /// <returns>获取到的id</returns>
+        public static long NewID()
+        {
+            if (generator == null)
+            {
+                GetInstance();
+            }
+            return generator.NewSequenceId();
+        }
+    }
+}
